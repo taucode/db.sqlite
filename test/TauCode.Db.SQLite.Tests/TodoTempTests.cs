bec9e693b3f819bd770a59d5a79e7f065a302787
Andrey Kovalenko <ak@taucode.com>
2020-09-24 14:25:29 +0300

    Added temp dummy tests.

---
+++ test/TauCode.Db.SQLite.Tests/TodoTempTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Db.SQLite.Tests
+{
+    [TestFixture]
+    public class TodoTempTests
+    {
+        [Test]
+        public void TodoTempTestDummy()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Pass("Added for CI init. Todo: remove asap.");
+        }
+    }
+}
